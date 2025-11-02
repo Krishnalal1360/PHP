5c401e512890cc6708a8f732edc75c859c96761d
Krishna Lal <krishnalal1360@gmail.com>
2025-11-02 18:06:14 +0530

    Array

--- Array/Array_List.php
+++ Array/Array_List.php
@@ -11,7 +11,7 @@
   echo "The value of \$d is $d <br>";
   echo "The value of \$e is $e <br>";*/
   //
-  $ar1 = [
+  /*$ar1 = [
           "a" => 10,
           "b" => 20,
           "c" => 30,
@@ -23,5 +23,29 @@
   echo "The value of b key is $b <br>";
   echo "The value of c key is $c <br>";
   echo "The value of d key is $d <br>";
-  echo "The value of e key is $e <br>";
+  echo "The value of e key is $e <br>";*/
+  //
+// Multi-dimensional array list
+$students = [
+    ["John", 20, "A"],
+    ["Alice", 22, "B+"],
+    ["Bob", 21, "A-"]
+];
+
+// Using foreach with key and list()
+foreach ($students as $index => list($name, $age, $grade)) {
+    echo "Index: $index | Name: $name | Age: $age | Grade: $grade<br>";
+}
+//
+// Multi-dimensional associative array
+$students = [
+    ["name" => "John",  "age" => 20, "grade" => "A"],
+    ["name" => "Alice", "age" => 22, "grade" => "B+"],
+    ["name" => "Bob",   "age" => 21, "grade" => "A-"]
+];
+
+// Using foreach with key and list()
+foreach ($students as $index => ["name" => $name, "age" => $age, "grade" => $grade]) {
+    echo "Index: $index | Name: $name | Age: $age | Grade: $grade<br>";
+}
 ?>
